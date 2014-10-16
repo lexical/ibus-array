ac269b30e79620875264e0296a3e58c6d410f99a
lexicall@gmail.com <lexicall@gmail.com@06f26268-b6d7-11de-ba48-051263943efc>
2014-10-16 10:38:43 +0000

    reformat for unified coding style.
    this patch does not add any new thing, just source code reformatting.

--- trunk/src/engine.c
+++ trunk/src/engine.c
@@ -25,53 +25,49 @@ struct _IBusArrayEngineClass {
 };
 
 /* functions prototype */
-static void	ibus_array_engine_class_init	(IBusArrayEngineClass	*klass);
-static void	ibus_array_engine_init		(IBusArrayEngine		*engine);
-static void	ibus_array_engine_destroy		(IBusArrayEngine		*engine);
-static gboolean 
-			ibus_array_engine_process_key_event
-                                            (IBusEngine             *engine,
-                                             guint               	 keyval,
-                                             guint                   keycode,
-                                             guint               	 modifiers);
-static void ibus_array_engine_focus_in    (IBusEngine             *engine);
-static void ibus_array_engine_focus_out   (IBusEngine             *engine);
-static void ibus_array_engine_reset       (IBusEngine             *engine);
-static void ibus_array_engine_enable      (IBusEngine             *engine);
-static void ibus_array_engine_disable     (IBusEngine             *engine);
-static void ibus_engine_set_cursor_location (IBusEngine             *engine,
-                                             gint                    x,
-                                             gint                    y,
-                                             gint                    w,
-                                             gint                    h);
-static void ibus_array_engine_set_capabilities
-                                            (IBusEngine             *engine,
-                                             guint                   caps);
-static void ibus_array_engine_page_up     (IBusEngine             *engine);
-static void ibus_array_engine_page_down   (IBusEngine             *engine);
-static void ibus_array_engine_cursor_up   (IBusEngine             *engine);
-static void ibus_array_engine_cursor_down (IBusEngine             *engine);
+static void ibus_array_engine_class_init (IBusArrayEngineClass *klass);
+static void ibus_array_engine_init (IBusArrayEngine	*engine);
+static void ibus_array_engine_destroy (IBusArrayEngine *engine);
+static gboolean ibus_array_engine_process_key_event
+                                            (IBusEngine *engine,
+                                             guint keyval,
+                                             guint keycode,
+                                             guint modifiers);
+static void ibus_array_engine_focus_in (IBusEngine *engine);
+static void ibus_array_engine_focus_out (IBusEngine *engine);
+static void ibus_array_engine_reset (IBusEngine *engine);
+static void ibus_array_engine_enable (IBusEngine *engine);
+static void ibus_array_engine_disable (IBusEngine *engine);
+static void ibus_engine_set_cursor_location (IBusEngine *engine,
+                                             gint x,
+                                             gint y,
+                                             gint w,
+                                             gint h);
+static void ibus_array_engine_set_capabilities (IBusEngine *engine, guint caps);
+static void ibus_array_engine_page_up (IBusEngine *engine);
+static void ibus_array_engine_page_down (IBusEngine *engine);
+static void ibus_array_engine_cursor_up (IBusEngine *engine);
+static void ibus_array_engine_cursor_down (IBusEngine *engine);
 static void ibus_array_engine_property_activate (IBusEngine *engine,
                                              const gchar *prop_name,
                                              gint prop_state);
-static void ibus_array_engine_property_show
-											(IBusEngine             *engine,
-                                             const gchar            *prop_name);
-static void ibus_array_engine_property_hide
-											(IBusEngine             *engine,
-                                             const gchar            *prop_name);
+static void ibus_array_engine_property_show (IBusEngine *engine,
+                                             const gchar *prop_name);
+static void ibus_array_engine_property_hide (IBusEngine *engine,
+                                             const gchar *prop_name);
 
-static void ibus_array_engine_commit_string
-                                            (IBusArrayEngine      *arrayeng,
-                                             const gchar            *string);
-static void ibus_array_engine_update      (IBusArrayEngine      *arrayeng);
+static void ibus_array_engine_commit_string (IBusArrayEngine *arrayeng,
+                                             const gchar *string);
+static void ibus_array_engine_update (IBusArrayEngine *arrayeng);
 static void ibus_array_engine_update_preedit (IBusArrayEngine *arrayeng);
 static void ibus_array_engine_update_lookup_table (IBusArrayEngine *arrayeng);
-static gboolean ibus_array_engine_process_candidate_key_event (IBusArrayEngine *arrayeng, guint keyval, guint modifiers);
+static gboolean ibus_array_engine_process_candidate_key_event (IBusArrayEngine *arrayeng,
+                                             guint keyval, guint modifiers);
 static gboolean ibus_array_engine_commit_current_candidate (IBusArrayEngine *arrayeng);
 static void ibus_array_engine_space_press(IBusArrayEngine *arrayeng);
 static gboolean ibus_array_engine_update_symbol_lookup_table (IBusArrayEngine *arrayeng);
-static void ibus_array_engine_update_auxiliary_text(IBusArrayEngine *arrayeng, gchar* aux_string);
+static void ibus_array_engine_update_auxiliary_text (IBusArrayEngine *arrayeng,
+                                             gchar* aux_string);
 
 static void ibus_array_engine_show_special_code(IBusArrayEngine *arrayeng);
 static void ibus_array_engine_show_special_code_for_char(IBusArrayEngine *arrayeng, gchar *ch);
@@ -105,21 +101,18 @@ ibus_array_engine_get_type (void)
 
 	static const GTypeInfo type_info = {
 		sizeof (IBusArrayEngineClass),
-		(GBaseInitFunc)		NULL,
+		(GBaseInitFunc)	NULL,
 		(GBaseFinalizeFunc) NULL,
-		(GClassInitFunc)	ibus_array_engine_class_init,
+		(GClassInitFunc) ibus_array_engine_class_init,
 		NULL,
 		NULL,
 		sizeof (IBusArrayEngine),
 		0,
-		(GInstanceInitFunc)	ibus_array_engine_init,
+		(GInstanceInitFunc) ibus_array_engine_init,
 	};
 
 	if (type == 0) {
-		type = g_type_register_static (IBUS_TYPE_ENGINE,
-									   "IBusArrayEngine",
-									   &type_info,
-									   (GTypeFlags) 0);
+		type = g_type_register_static (IBUS_TYPE_ENGINE, "IBusArrayEngine", &type_info, (GTypeFlags) 0);
 	}
 
 	return type;
@@ -167,12 +160,12 @@ ibus_array_exit (void)
 static void
 ibus_array_engine_class_init (IBusArrayEngineClass *klass)
 {
-	IBusObjectClass *ibus_object_class = IBUS_OBJECT_CLASS (klass);
-	IBusEngineClass *engine_class = IBUS_ENGINE_CLASS (klass);
+    IBusObjectClass *ibus_object_class = IBUS_OBJECT_CLASS (klass);
+    IBusEngineClass *engine_class = IBUS_ENGINE_CLASS (klass);
 
-	parent_class = (IBusEngineClass *) g_type_class_peek_parent (klass);
+    parent_class = (IBusEngineClass *) g_type_class_peek_parent (klass);
 	
-	ibus_object_class->destroy = (IBusObjectDestroyFunc) ibus_array_engine_destroy;
+    ibus_object_class->destroy = (IBusObjectDestroyFunc) ibus_array_engine_destroy;
 
     engine_class->process_key_event = ibus_array_engine_process_key_event;
     engine_class->reset = ibus_array_engine_reset;
@@ -253,13 +246,13 @@ static void ibus_array_engine_reset(IBusEngine *engine) {
 static void
 ibus_array_engine_page_up (IBusEngine *engine)
 {
-        parent_class->page_up (engine);
+    parent_class->page_up (engine);
 }
 
 static void
 ibus_array_engine_page_down (IBusEngine *engine)
 {
-        parent_class->page_down (engine);
+    parent_class->page_down (engine);
 }
 
 static void ibus_array_engine_focus_in (IBusEngine *engine)
@@ -328,7 +321,6 @@ ibus_array_engine_update_preedit (IBusArrayEngine *arrayeng)
 
     text = ibus_text_new_from_string(array_preedit->str);
 
-    //text = ibus_text_new_from_static_string (arrayeng->preedit->str);
     text->attrs = ibus_attr_list_new ();
 
     ibus_attr_list_append (text->attrs,
@@ -344,7 +336,7 @@ ibus_array_engine_update_preedit (IBusArrayEngine *arrayeng)
     
     ibus_engine_update_preedit_text ((IBusEngine *)arrayeng,
                                      text,
-                                     array_preedit->len, //arrayeng->cursor_pos,
+                                     array_preedit->len,
                                      TRUE);
     if (G_IS_OBJECT (text) && g_object_is_floating (text))
     	g_object_unref (text);
@@ -453,34 +445,19 @@ ibus_array_engine_update (IBusArrayEngine *arrayeng)
     ibus_array_engine_show_special_code(arrayeng);
 }
 
-//#define is_alpha(c) (((c) >= IBUS_a && (c) <= IBUS_z) || ((c) >= IBUS_A && (c) <= IBUS_Z))
 #define is_alpha(c) (((c) >= IBUS_a && (c) <= IBUS_z))
 
 static gboolean 
 ibus_array_engine_process_key_event (IBusEngine *engine,
-                                       guint       keyval,
-                                       guint       keycode,
-                                       guint       modifiers)
+                                       guint keyval,
+                                       guint keycode,
+                                       guint modifiers)
 {
     IBusText *text;
     IBusArrayEngine *arrayeng = (IBusArrayEngine *)engine;
 
     if (modifiers & IBUS_RELEASE_MASK)
         return FALSE;
-
-    //modifiers &= (IBUS_CONTROL_MASK | IBUS_MOD1_MASK);
-
-    /*if (modifiers == IBUS_CONTROL_MASK && keyval == IBUS_s) {
-        ibus_array_engine_update_lookup_table (arrayeng);
-        return TRUE;
-    }*/
-
-    /*if (modifiers != 0) {
-        if (arrayeng->preedit->len == 0)
-            return FALSE;
-        else
-            return TRUE;
-    }*/
 
     if (keyval == IBUS_Shift_L || keyval == IBUS_Shift_R)
         return FALSE;
@@ -491,22 +468,17 @@ ibus_array_engine_process_key_event (IBusEngine *engine,
 
     switch (keyval) {
     case IBUS_space:
-        //g_string_append (arrayeng->preedit, " ");
         if (arrayeng->preedit->len == 0)
             return FALSE;
         ibus_array_engine_space_press(arrayeng);
         return TRUE;
-        //return ibus_array_engine_commit_preedit (arrayeng);
     case IBUS_Return:
         if (arrayeng->preedit->len == 0)
             return FALSE;
         else
             return TRUE;
-        //return ibus_array_engine_commit_preedit (arrayeng);
 
     case IBUS_Escape:
-        /*if (arrayeng->preedit->len == 0)
-            return FALSE;*/
         ibus_array_engine_reset((IBusEngine*)arrayeng);
         return TRUE;        
 
@@ -527,12 +499,6 @@ ibus_array_engine_process_key_event (IBusEngine *engine,
         ibus_lookup_table_page_down (arrayeng->table);
         ibus_engine_update_lookup_table ((IBusEngine *) arrayeng, arrayeng->table, TRUE);
         return TRUE;
-        /*if (arrayeng->cursor_pos < arrayeng->preedit->len) {
-            arrayeng->cursor_pos ++;
-            ibus_array_engine_update (arrayeng);
-        }
-        return TRUE;*/
-    
 
     case IBUS_BackSpace:
     case IBUS_Delete:
@@ -567,9 +533,6 @@ ibus_array_engine_process_key_event (IBusEngine *engine,
                 ibus_lookup_table_set_cursor_pos (arrayeng->table, 0);
 
                 commit_rev = ibus_array_engine_commit_current_candidate(arrayeng);
-
-                /*if (commit_rev)
-                    ibus_array_engine_reset((IBusEngine*)arrayeng);*/
             } else {
 		ibus_array_engine_reset((IBusEngine*)arrayeng);
             }
@@ -579,9 +542,7 @@ ibus_array_engine_process_key_event (IBusEngine *engine,
                 return TRUE;
         }
 
-        g_string_insert_c (arrayeng->preedit,
-                           arrayeng->cursor_pos,
-                           keyval);
+        g_string_insert_c (arrayeng->preedit, arrayeng->cursor_pos, keyval);
 
         arrayeng->cursor_pos ++;
         ibus_array_engine_update (arrayeng);
@@ -617,9 +578,6 @@ ibus_array_engine_process_candidate_key_event (IBusArrayEngine *arrayeng,
         ibus_lookup_table_set_cursor_pos (arrayeng->table, cursor_pos);
 
         commit_rev = ibus_array_engine_commit_current_candidate(arrayeng);
-
-        /*if (commit_rev)
-            ibus_array_engine_reset((IBusEngine*)arrayeng);*/
     }
 
     return TRUE;
@@ -644,13 +602,6 @@ static void ibus_array_engine_space_press(IBusArrayEngine *arrayeng) {
 
             ibus_lookup_table_set_cursor_pos(arrayeng->table, 0);
             commit_rev = ibus_array_engine_commit_current_candidate(arrayeng);
-
-            /*if (commit_rev) {
-                ibus_array_engine_reset((IBusEngine*)arrayeng);
-            }
-            else {
-                ibus_engine_hide_lookup_table((IBusEngine*)arrayeng);
-            }*/
         }
     }
     else if (arrayeng->space_press_count == 1) {
@@ -661,15 +612,8 @@ static void ibus_array_engine_space_press(IBusArrayEngine *arrayeng) {
             ibus_lookup_table_set_cursor_pos (arrayeng->table, 0);
 
             commit_rev = ibus_array_engine_commit_current_candidate(arrayeng);
-
-            /*if (commit_rev)
-                ibus_array_engine_reset((IBusEngine*)arrayeng);
-            else {
-                ibus_engine_hide_lookup_table((IBusEngine*)arrayeng);
-             }*/
         }
         else {
-            //ibus_engine_hide_lookup_table((IBusEngine*)arrayeng);
             ibus_array_engine_reset((IBusEngine*)arrayeng);
         }
     }
