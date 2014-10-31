08b6c77cc6a822c0cdecbd4a1155dac05b7fb7ae
lexicall@gmail.com <lexicall@gmail.com@06f26268-b6d7-11de-ba48-051263943efc>
2014-10-31 14:50:40 +0000

    Use Gobject introspection in the setup dialog
    Both GTK and ibus should use this. dependency on ibus-python is dropped

--- setup/main.py
+++ setup/main.py
@@ -23,30 +23,30 @@
  
 import sys
 import os
-import gobject
-import gtk
-import ibus
+from gi.repository import GLib
+from gi.repository import Gtk
+from gi.repository import IBus
 import gettext
 import config
 
 class Setup:
-    def __init__(self):
-        self.__bus = ibus.Bus()
+    def __init__(self, bus):
+        self.__bus = bus 
         self.__config = self.__bus.get_config()
         self.__config.connect("value-changed", self.on_value_changed, None)
         self.__create_ui()
 
     def __create_ui(self):
-        self.__window = gtk.Dialog('ibus-array setup', None, 
-                                    gtk.DIALOG_MODAL, 
-                                    (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, 
-                                     gtk.STOCK_OK, gtk.RESPONSE_OK)
+        self.__window = Gtk.Dialog('ibus-array setup', None, 
+                                    Gtk.DialogFlags.MODAL, 
+                                    (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, 
+                                     Gtk.STOCK_OK, Gtk.ResponseType.OK)
                                   )
         icon_file = os.path.join(config.datadir, "ibus-array", "icons", "ibus-array.png")
         self.__window.set_icon_from_file(icon_file)
-        self.__special_notify_button = gtk.CheckButton("Special Code Notification")
+        self.__special_notify_button = Gtk.CheckButton("Special Code Notification")
         self.__window.vbox.pack_start(self.__special_notify_button, True, True, 10)
-        self.__special_only_button = gtk.CheckButton("Speical Code Only Mode")
+        self.__special_only_button = Gtk.CheckButton("Speical Code Only Mode")
         self.__window.vbox.pack_start(self.__special_only_button, True, True ,10)
 
         current_special_mode = self.__read("SpecialOnly", "0")
@@ -61,7 +61,7 @@ class Setup:
 
     def run(self):
         res = self.__window.run()
-        if res == gtk.RESPONSE_OK:
+        if res == Gtk.ResponseType.OK:
             self.apply()
         self.__window.destroy()
 
@@ -70,13 +70,13 @@ class Setup:
         select_special_mode = self.__special_only_button.get_active()
 
         if select_special_notify:
-            self.__write("SpecialNotify", "1")
+            self.__write("SpecialNotify", GLib.Variant.new_string("1"))
         else:
-            self.__write("SpecialNotify", "0")
+            self.__write("SpecialNotify", GLib.Variant.new_string("0"))
         if select_special_mode:
-            self.__write("SpecialOnly", "1")
+            self.__write("SpecialOnly", GLib.Variant.new_string("1"))
         else:
-            self.__write("SpecialOnly", "0")
+            self.__write("SpecialOnly", GLib.Variant.new_string("0"))
 
     def on_value_changed(self, config, section, name, value, data):
         if section == 'engine/Array':
@@ -92,10 +92,15 @@ class Setup:
                     self.__special_notify_button.set_active(False)
 
     def __read(self, name, v):
-        return self.__config.get_value("engine/Array", name, v)
+	value = self.__config.get_value("engine/Array", name)
+	if value is None:
+		return v
+        return value
 
     def __write(self, name, v):
         return self.__config.set_value("engine/Array", name, v)
 
 if __name__ == '__main__':
-    Setup().run()
+    bus = IBus.Bus()
+    if bus.is_connected():
+        Setup(bus).run()
