8a7f89f4484a2cd63e12ef3dd8409c803d369f5f
lexicall@gmail.com <lexicall@gmail.com@06f26268-b6d7-11de-ba48-051263943efc>
2014-11-06 11:21:11 +0000

    update the copyright

--- trunk/setup/main.py
+++ trunk/setup/main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# vim:set et sts=4 sw=4:
-#
 # ibus-array - The Array 30 Engine for IBus
 #
 # Copyright (c) 2009 Yu-Chun Wang <mainlander1122@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2, or (at your option)
+# the Free Software Foundation; either version 2, or
 # any later version.
 #
 # This program is distributed in the hope that it will be useful,
--- trunk/src/array.h
+++ trunk/src/array.h
@@ -1,3 +1,24 @@
+/*
+ ibus-array - The Array 30 Engine for IBus
+
+ Copyright (c) 2009-2014 Yu-Chun Wang <mainlander1122@gmail.com>
+                         Keng-Yu Lin <kengyu@lexical.tw>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 #ifndef _ARRAY_HEADER_
 #define _ARRAY_HEADER_
 
